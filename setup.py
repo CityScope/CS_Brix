e9aec519e6c80d059ca0f521dbbe654c371d265d
crisjf <crisjf@mit.edu>
2020-09-07 22:31:43 -0400

    changed license type to GNU GENERAL PUBLIC LICENSE  in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cs-brix", # Replace with your own username
+    name="cs-brix",
     version="0.0.1",
     author="CityScience MIT",
     author_email="csadmin@media.mit.edu",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: GNU GENERAL PUBLIC LICENSE",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
