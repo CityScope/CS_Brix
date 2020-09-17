d2f7786f4f369cf514034a37ca6f85a4de0730fa
crisjf <crisjf@mit.edu>
2020-09-17 18:37:16 -0400

    removed old build_documentation file, things are now handled by action;

--- docsource/source/build_documentation.sh
+++
@@ -1,5 +0,0 @@
-sphinx-build -b html ./ ../../docs
-sphinx-build -M markdown ./ ../../docs
-printf "\n\n" >> ../../docs/markdown/README.md
-mv ../../docs/markdown/README.md ../../README.md
-cat ../../README.md  ../../docs/markdown/classes.md > ../../docs/markdown/docusaurus.md
