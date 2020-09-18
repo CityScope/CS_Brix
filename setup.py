46c6a1fac259e278d0952179665395c9bd033f02
crisjf <crisjf@mit.edu>
2020-09-18 20:52:05 +0000

    Documentation automatically updated by GitHub action

--- setup.py
+++ setup.py
@@ -6,17 +6,32 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="cs-brix",
     version="0.0.1",
+    license='GNU General Public License v3.0 only (GPL-3.0-only)',
     author="CityScience MIT",
     author_email="csadmin@media.mit.edu",
-    description="Set of tools to deploy CityScope indicators.",
+    description="Brix is a python library for CityScope modules which handles communication with City I/O.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CityScope/CS_Brix",
+    download_url = 'https://github.com/CityScope/CS_Brix/archive/v_0.0.1.tar.gz',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'requests>=2.22',
+        'pandas>=0.25.3',
+        'Geohash>=1.0',
+        'python-geohash>=0.8.5',
+        'joblib>=0.13.2',
+        'shapely>=1.6.2',
+        'geopandas>=0.6.2',
+        'geopy>=1.20.0'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: GNU GENERAL PUBLIC LICENSE",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent"
     ],
     python_requires='>=3.6',
 )
