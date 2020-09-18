cf7fb77847b16196e62d4383d965f32bad00ff13
crisjf <crisjf@mit.edu>
2020-09-18 16:31:20 -0400

    package installed as cs-brix and imported as brix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="brix",
+    name="cs-brix",
     version="0.0.1",
     license='GNU GENERAL PUBLIC LICENSE',
     author="CityScience MIT",
