56649f238042fad91f0992b4e6b0f092385ea191
crisjf <crisjf@mit.edu>
2020-12-10 19:03:48 -0500

    added add_height function to test posts to GEOGRIDDATA #19

--- brix/functions.py
+++ brix/functions.py
@@ -6,6 +6,22 @@ from .classes import CompositeIndicator
 from .classes import GEOGRIDDATA
 
 def add_height(geogrid_data, levels):
+  '''
+  Adds levels to all the cells in geogrid.
+  Function mainly used for testing as an example. 
+
+  Parameters
+  ---------
+  geogrid_data: dict or :class:`brix.GEOGRIDDATA`
+    List of dicts with the geogrid_data information.
+  levels: float
+    Number of levels by which to rise height
+  
+  Returns
+  -------
+  new_geogrid_data: dict
+    Same as input, but with additional levels in each cell.
+  '''
   for cell in geogrid_data:
     cell['height'] += levels
   return geogrid_data
