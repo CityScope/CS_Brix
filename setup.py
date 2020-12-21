96091906740be06813c0f44e5b1cb355d92dd896
crisjf <crisjf@mit.edu>
2020-12-21 13:35:27 -0500

    bumping to 0.0.5 after fixting install issue

--- setup.py
+++ setup.py
@@ -3,12 +3,24 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as f:
-    INSTALL_REQUIRES = [line.strip() for line in f.readlines()]
+try:
+    with open("requirements.txt") as f:
+        INSTALL_REQUIRES = [line.strip() for line in f.readlines()]
+except:
+    INSTALL_REQUIRES = [
+        "requests>=2.24",
+        "pandas>=0.25.3",
+        "pygeohash>=1.2.0",
+        "joblib>=0.13.2",
+        "shapely>=1.6.2",
+        "geopandas>=0.6.2",
+        "geopy>=1.20.0"
+    ]
+
 
 setuptools.setup(
     name="cs-brix",
-    version="0.0.4",
+    version="0.0.5",
     license='GNU General Public License v3.0 only (GPL-3.0-only)',
     author="CityScience MIT",
     author_email="csadmin@media.mit.edu",
@@ -16,7 +28,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CityScope/CS_Brix",
-    download_url = 'https://github.com/CityScope/CS_Brix/archive/v_0.0.4.tar.gz',
+    download_url = 'https://github.com/CityScope/CS_Brix/archive/v_0.0.5.tar.gz',
     packages=setuptools.find_packages(),
     install_requires=INSTALL_REQUIRES,
     classifiers=[
