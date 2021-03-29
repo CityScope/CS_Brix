6681f90b979c6eb724616e850d41147925fb251f
crisjf <crisjf@mit.edu>
2021-03-29 11:38:09 -0400

    added Content-Type:application/json to all post requests #36

--- brix/classes.py
+++ brix/classes.py
@@ -360,30 +360,16 @@ class Handler(Thread):
 
 		super(Handler, self).__init__()
 
-<<<<<<< HEAD
-		
 		self.host = 'https://cityiotest.mirage.city' # 'https://cityio.media.mit.edu'
 		self.host = 'http://127.0.0.1:5000/' if host_mode=='local' else self.host
-		self.post_headers = {'Content-type': 'application/json'}
-=======
-		if host_mode=='local':
-			self.host = 'http://127.0.0.1:5000/'
-		else:
-			# self.host = 'https://cityio.media.mit.edu'
-			self.host = 'https://cityiotest.mirage.city'
-		self.table_name = table_name
-		self.quietly = quietly
->>>>>>> created urljoin function to ensure trailing slashes are always there #36
+		self.post_headers = {'Content-Type': 'application/json'}
 
 		self.sleep_time = 0.5
 		self.nAttempts = 5
 		self.append_on_post = False
 
-<<<<<<< HEAD
 		self.table_name = table_name
 	
-=======
->>>>>>> created urljoin function to ensure trailing slashes are always there #36
 		self.front_end_url   = f'https://cityscope.media.mit.edu/CS_cityscopeJS/?cityscope={self.table_name}'
 		self.cityIO_get_url  = urljoin(self.host,'api/table',self.table_name)
 		self.cityIO_post_url = urljoin(self.host,'api/table',self.table_name)
@@ -1197,25 +1183,16 @@ class Handler(Thread):
 		''':class:`brix.Handler` keeps track of the previous value of the indicators and access values.This function rollsback the current values to whatever the locally stored values are.
 		See also :func:`brix.Handler.previous_indicators` and :func:`brix.Handler.previous_access`.
 		'''
-<<<<<<< HEAD
 		r = requests.post(urljoin(self.cityIO_post_url,'indicators'), data=json.dumps(self.previous_indicators), headers=self.post_headers)
 		r = requests.post(urljoin(self.cityIO_post_url,'access'),     data=json.dumps(self.previous_access),     headers=self.post_headers)
-=======
-		r = requests.post(urljoin(self.cityIO_post_url,'indicators'), data = json.dumps(self.previous_indicators))
-		r = requests.post(urljoin(self.cityIO_post_url,'access'),     data = json.dumps(self.previous_access))
->>>>>>> created urljoin function to ensure trailing slashes are always there #36
+
 
 	def clear_table(self):
 		'''Clears all indicators from the table.'''
 		grid_hash_id = self.get_grid_hash()
 		empty_update = {'numeric': [],'heatmap': {'type': 'FeatureCollection', 'properties': [], 'features': []}}
-<<<<<<< HEAD
 		r = requests.post(urljoin(self.cityIO_post_url,'indicators'), data=json.dumps(empty_update['numeric']), headers=self.post_headers)
 		r = requests.post(urljoin(self.cityIO_post_url,'access')    , data=json.dumps(empty_update['heatmap']), headers=self.post_headers)
-=======
-		r = requests.post(urljoin(self.cityIO_post_url,'indicators'), data = json.dumps(empty_update['numeric']))
-		r = requests.post(urljoin(self.cityIO_post_url,'access')    , data = json.dumps(empty_update['heatmap']))
->>>>>>> created urljoin function to ensure trailing slashes are always there #36
 		if not self.quietly:
 			print('Cleared table')
 		self.grid_hash_id = grid_hash_id
@@ -1338,11 +1315,7 @@ class Handler(Thread):
 		ids = np.argsort(ids)
 		geogrid_data = [geogrid_data[i] for i in ids]
 
-<<<<<<< HEAD
 		r = requests.post(urljoin(self.cityIO_post_url,self.GEOGRIDDATA_varname), data=json.dumps(geogrid_data), headers=self.post_headers)
-=======
-		r = requests.post(urljoin(self.cityIO_post_url,self.GEOGRIDDATA_varname), data=json.dumps(geogrid_data))
->>>>>>> created urljoin function to ensure trailing slashes are always there #36
 		self.grid_hash_id = self.get_grid_hash()
 		if not self.quietly:
 			print('GEOGRIDDATA successfully updated:',self.grid_hash_id)
