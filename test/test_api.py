e12f25cadb835951f942c5f4ff36a044d123f80f
crisjf <crisjf@mit.edu>
2021-03-31 09:57:48 -0400

    unittest for new api

--- test/test_api.py
+++ test/test_api.py
@@ -8,76 +8,96 @@ import json
 class TestEndpoints(unittest.TestCase):
 
 	def setUp(self):
-		self.table_list = []
-
-	def tearDown(self):
-		print('Cleaning up')
-		for table_name in self.table_list:
-			print('Deleting:',table_name)
-			H = Handler(table_name,shell_mode=True)
-			r = requests.delete(H.cityIO_get_url)
-
-	def test_pipeline(self):
-		'''
-		Test with a random table from cityio
-		'''
-		
+		# Pick a vaild table from cityio.media to test with
 		r = requests.get('https://cityio.media.mit.edu/api/tables/list/')
 		table_list = r.json()
-
 		valid_table = False
 		while not valid_table:
 			raw_table_name = random.choice(table_list).strip('/').split('/')[-1]
 			table_name = f'{raw_table_name}_brix_test_table'
 			r = requests.get(f'https://cityio.media.mit.edu/api/table/{raw_table_name}')
-
 			if (r.status_code==200) and (r.headers['Content-Type']=='application/json'):
 				table_data = r.json()
 				if (table_data!='access restricted') and ('GEOGRID' in table_data.keys()):
 					valid_table = True
-				
-		self.table_list.append(table_name)
+
+		self.table_data = table_data
+		self.table_name = table_name
+
+		H = Handler(self.table_name,shell_mode=True)
+		self.cityIO_post_url = H.cityIO_post_url
+		self.cityIO_get_url  = H.cityIO_get_url
+		self.post_headers    = H.post_headers
 
 		print('Testing with table:',raw_table_name,'as',table_name)
-		H = Handler(table_name,shell_mode=True)
 
-		# Test table create
-		print('Creating table')
-		r = requests.post(H.cityIO_post_url, data=json.dumps(table_data), headers=H.post_headers)
-		self.assertEqual(r.status_code,200)
 
-		# Test get
-		print('Testing GET for entire table')
-		r = requests.get(H.cityIO_get_url)
-		self.assertEqual(r.status_code,200)
-		self.assertEqual(r.headers['Content-Type'],'application/json')
+	def tearDown(self):
+		print('Cleaning up')
+		print('Deleting:',self.table_name)
+		r = requests.delete(self.cityIO_get_url)
 
-		# Test post
-		print('Testing POST for entire table')
-		if (r.status_code==200) and (r.headers['Content-Type']=='application/json'):
-			data = r.json()
-			r = requests.post(H.cityIO_post_url, data=json.dumps(data), headers=H.post_headers)
-			self.assertEqual(r.status_code,200)
+	def test_create_table(self):
+		r = requests.post(self.cityIO_post_url, data=json.dumps(self.table_data), headers=self.post_headers)
+		self.assertEqual(
+			r.status_code,200,
+			msg=f"test_create_table Failed: table_name={self.table_name}, status_code={r.status_code}"
+		)
 
+	def test_get(self):
+		# Test get for entire table
+		r = requests.post(self.cityIO_post_url, data=json.dumps(self.table_data), headers=self.post_headers)
+		self.assertEqual(
+			r.status_code,200,
+			msg=f'test_get Failed, table could not be posted: table_name={self.table_name}, status_code={r.status_code}'
+		)
+		r = requests.get(self.cityIO_get_url)
+		self.assertEqual(
+			r.status_code,200,
+			msg=f'test_get Failed: table_name={self.table_name}, status_code={r.status_code}, url={self.cityIO_get_url}'
+		)
+		self.assertEqual(
+			r.headers['Content-Type'],'application/json',
+			msg=f"test_get Failed with wrong Content-Type: table_name={self.table_name}, content_type={r.headers['Content-Type']}, url={self.cityIO_get_url}"
+		)
+
+	def test_deep_get_post(self):
 		# Test deep get and deep post
+		r = requests.post(self.cityIO_post_url, data=json.dumps(self.table_data), headers=self.post_headers)
+		self.assertEqual(
+			r.status_code,200,
+			msg=f'test_deep_get_post Failed, table could not be posted: table_name={self.table_name}, status_code={r.status_code}'
+		)
+
 		print('Testing deep get and deep post')
-		for branch in table_data.keys():
-			print(f'\tTesting GET for {branch}')
-			r = requests.get(urljoin(H.cityIO_get_url,branch))
-			self.assertEqual(r.status_code,200)
+		for branch in self.table_data.keys():
+			url = urljoin(self.cityIO_get_url,branch)
+			r = requests.get(url)
+			self.assertEqual(
+				r.status_code,200,
+				msg=f'test_deep_get_post GET Failed: table_name={self.table_name}, branch={branch}, status_code={r.status_code}, url={url}'			
+			)
 			if (r.status_code==200) and (r.headers['Content-Type']=='application/json'):
 				data = r.json()
-				print(f'\tTesting POST for {branch}')
-				r = requests.post(urljoin(H.cityIO_post_url,branch), data=json.dumps(data), headers=H.post_headers)
+				r = requests.post(urljoin(self.cityIO_post_url,branch), data=json.dumps(data), headers=self.post_headers)
 				if branch!='meta':
-					self.assertEqual(r.status_code,200)
+					self.assertEqual(
+						r.status_code,200,
+						msg=f'test_deep_get_post POST Failed: table_name={self.table_name}, branch={branch}, status_code={r.status_code}'
+					)
 				else:
-					self.assertEqual(r.status_code,406)
-		
+					self.assertEqual(
+						r.status_code,406,
+						msg=f'test_deep_get_post POST Failed for meta branch: table_name={self.table_name}, branch={branch}, status_code={r.status_code}'
+					)
+
+	def table_delete(self):
 		# Test table deletion
-		print(f'Testing table deletion')
-		r = requests.delete(H.cityIO_get_url)
-		self.assertEqual(r.status_code,200)
+		r = requests.delete(self.cityIO_get_url)
+		self.assertEqual(
+			r.status_code,200,
+			msg=f'table_delete Failed: table_name={self.table_name}, status_code={r.status_code}, url={self.cityIO_get_url}'
+		)
 
 
 if __name__ == '__main__':
