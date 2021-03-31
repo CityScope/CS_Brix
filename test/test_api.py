ff3fa9ab5518cbd8eb2498dd2092dd9f38bb603a
crisjf <crisjf@mit.edu>
2021-03-31 10:18:11 -0400

    ensure we are not overwriting a table in test server

--- test/test_api.py
+++ test/test_api.py
@@ -15,21 +15,24 @@ class TestEndpoints(unittest.TestCase):
 	def setUp(self):
 		'''
 		Pick a random valid table from cityio.media to clone.
-		Valid tables have at least a GEOGRID endpoint and are not restricted access.
+		Valid tables have at least a GEOGRID endpoint, are not restricted access, and do not exist in cityiotest.
 		Uses the get and post urls from brix.Handler. This also tests brix's behavior. 
 
 		'''
 		r = requests.get('https://cityio.media.mit.edu/api/tables/list/')
 		table_list = r.json()
+		r = requests.get('https://cityiotest.mirage.city/api/tables/list/')
+		table_list_mirage = r.json()
 		valid_table = False
 		while not valid_table:
 			raw_table_name = random.choice(table_list).strip('/').split('/')[-1]
-			table_name = f'{raw_table_name}_brix_test_table'
-			r = requests.get(f'https://cityio.media.mit.edu/api/table/{raw_table_name}')
-			if (r.status_code==200) and (r.headers['Content-Type']=='application/json'):
-				table_data = r.json()
-				if (table_data!='access restricted') and ('GEOGRID' in table_data.keys()):
-					valid_table = True
+			table_name = f'{raw_table_name}_brix_table_for_unittest'
+			if table_name not in table_list_mirage:
+				r = requests.get(f'https://cityio.media.mit.edu/api/table/{raw_table_name}')
+				if (r.status_code==200) and (r.headers['Content-Type']=='application/json'):
+					table_data = r.json()
+					if (table_data!='access restricted') and ('GEOGRID' in table_data.keys()):
+						valid_table = True
 
 		self.table_data = table_data
 		self.table_name = table_name
@@ -46,8 +49,7 @@ class TestEndpoints(unittest.TestCase):
 		'''
 		Delete the table that we tested with.
 		'''
-		print('Cleaning up')
-		print('Deleting:',self.table_name)
+		print('Cleaning up:',self.table_name)
 		r = requests.delete(self.cityIO_get_url)
 
 	def test_create_table(self):
