a263a51f5641445c9c88f1a9f43d58f7a5962ebf
crisjf <crisjf@mit.edu>
2021-05-06 11:35:35 -0400

    merged changes from table_generator.py #40

--- brix/grid_maker.py
+++ brix/grid_maker.py
@@ -4,7 +4,11 @@
 Created on Mon Jul 29 18:57:59 2019
 
 @author: doorleyr
+@contributors: crisjf, guadalupebabio
 """
+from .classes import Handler
+from .helpers import deg_to_rad,rad_to_deg
+
 import pyproj
 import math
 import numpy as np
@@ -12,39 +16,10 @@ import matplotlib.path as mplPath
 import requests
 import matplotlib.pyplot as plt
 
-def deg_to_rad(deg):
-    return deg*math.pi/180
-
-def rad_to_deg(rad):
-    return rad*180/math.pi
-
-def point_in_shape(point, geometry):
-    if geometry['type']=='MultiPolygon':
-        for polygon in geometry['coordinates'][0]:
-            bbPath = mplPath.Path(polygon)
-            if bbPath.contains_point((point[0],point[1])):
-                return True
-        return False
-    else:
-        bbPath = mplPath.Path(geometry['coordinates'][0])
-        if bbPath.contains_point((point[0],point[1])):
-            return True
-        else:
-            return False
-            
-#def flip_geojson_vertical(geojson, n_rows, n_cols):
-#    new_features=[]
-#    for row in reversed(range(n_rows)):
-#        new_features.extend(geojson['features'][row*ncols: (row+1)*ncols])
-#    geojson['features']=new_features
-#    return geojson
-        
-            
-
 wgs=pyproj.Proj("+init=EPSG:4326")
 
 class Grid():
-    def __init__(self, top_left_lon, top_left_lat, rotation, crs_epsg, 
+    def __init__(self, table_name, top_left_lon, top_left_lat, rotation, crs_epsg, 
                  cell_size, nrows, ncols, flip_y=False):
         """
         Takes the properties of the grid and using the Haversine formula, 
@@ -88,68 +63,21 @@ class Grid():
         lon_grid, lat_grid=pyproj.transform(self.projection,wgs,x_rot_trans, y_rot_trans)
         self.grid_coords_ll=[[lon_grid[i], lat_grid[i]] for i in range(len(lon_grid))]
         self.grid_coords_xy=[[x_rot_trans[i], y_rot_trans[i]] for i in range(len(y_rot_trans))]
-        self.properties={'tui_id': [None for g in lon_grid],
-                         'interactive':[True for g in lon_grid]}
-        self.geogrid_to_tui_mapping={}
-        self.header={'longitude': top_left_lon,
+        self.properties={'color':[0,0,0],
+                        'height': 0,
+                        'id': 0, 
+                        'interactive': "Web",
+                        'name': "test"}
+        self.header={'cellSize': cell_size,
             'latitude': top_left_lat,
+            'longitude': top_left_lon,
             'ncols':ncols,
             'nrows': nrows,
-            'cellSize': cell_size,
-            'rotation': rotation}
-    
-    def flip_tui_ids_y(self):
-        new_tui_ids=[]
-        new_tui_mapping={}
-        for row in reversed(range(self.nrows)):
-            new_tui_ids.extend(self.properties['tui_id'][row*self.ncols: (row+1)*self.ncols])
-        for grid_cell_id in range(len(new_tui_ids)):
-            if new_tui_ids[grid_cell_id] is not None:
-                new_tui_mapping[grid_cell_id]=  new_tui_ids[grid_cell_id]
-        self.properties['tui_id']=new_tui_ids
-        self.geogrid_to_tui_mapping=new_tui_mapping
-        
-    def add_tui_interactive_cells(self, tui_top_left_row_index, tui_top_left_col_index,
-                                  tui_num_interactive_rows, tui_num_interactive_cols):
-        """
-        takes an initialised grid which defines the full model area
-        assigns a portion of the rows and coolumsn to be TUI interactive cells
-        the  interactive cells are maked as interactive and given a grid_data_id
-        corresponding to their order in the interactive grid.
-        
-        """    
-        
-        tui_ind=0
-        for r in range(tui_top_left_row_index, tui_top_left_row_index+tui_num_interactive_rows):
-            for c in range(tui_top_left_col_index, tui_top_left_col_index+tui_num_interactive_cols):
-                geogrid_index=r*self.ncols+c
-                self.properties['tui_id'][geogrid_index]=tui_ind
-                self.geogrid_to_tui_mapping[geogrid_index]=tui_ind
-                tui_ind+=1
-                
-    def set_web_interactive_region(self, interactive_zone_geojson):
-        interactive=[False]*len(self.grid_coords_ll)
-        for cell_num in range(len(self.grid_coords_ll)):
-            for lu_feature in interactive_zone_geojson['features']:
-                if point_in_shape(self.grid_coords_ll[cell_num], lu_feature['geometry']):
-                    interactive[cell_num]=True
-        self.properties['interactive']=interactive
-        
-        
-        
-    def set_grid_properties_from_shapefile(self, ref_geojson, properties):
-        """
-        Takes a grid with interactive and static cells
-        Cross-references each cell to a geojson file of land-use polygons
-        Assigns each cell to a land-use
-        """
-        new_properties={attr: [properties[attr]['default']]*len(self.grid_coords_ll) for attr in properties}
-        for cell_num in range(len(self.grid_coords_ll)):
-            for ref_feature in ref_geojson['features']:
-                if point_in_shape(self.grid_coords_ll[cell_num], ref_feature['geometry']):
-                    for attr in properties:
-                        new_properties[attr][cell_num]=ref_feature['properties'][properties[attr]['from']]
-        self.properties.update(new_properties)
+            'projection': '+proj=lcc +lat_1=42.68333333333333 +lat_2=41.71666666666667 +lat_0=41 +lon_0=-71.5 +x_0=200000 +y_0=750000 +ellps=GRS80 +datum=NAD83 +units=m +no_def', #not sure from where?
+            'rotation': rotation,
+            'tableName':table_name,
+            'tz': -5 
+            }
 
     def get_grid_geojson(self, add_properties={}, include_global_properties=True):
         """
@@ -160,7 +88,8 @@ class Grid():
                          self.grid_coords_ll[1][1]-self.grid_coords_ll[0][1]]
         dLLdRow=[self.grid_coords_ll[self.ncols][0]-self.grid_coords_ll[0][0], 
                        self.grid_coords_ll[self.ncols][1]-self.grid_coords_ll[0][1]]
-        features=[]       
+        features=[]      
+        print(self.grid_coords_ll) 
         for i, g in enumerate(self.grid_coords_ll):
             coords=[g, 
                     [g[0]+dLLdRow[0], g[1]+dLLdRow[1]],
@@ -169,19 +98,33 @@ class Grid():
                     [g[0]+dLLdCol[0],g[1]+dLLdCol[1]], 
                     g]
             properties={}
-            for p in self.properties:
-                properties[p]=self.properties[p][i]
-            for p in add_properties:
-                properties[p]=add_properties[p][i]
+            print(self.properties)
+            properties=copy.copy(self.properties)
+            properties['id'] = i
+            # for p in self.properties:
+            #     properties[p]=self.properties[p][i]
+            # for p in add_properties:
+            #     properties[p]=add_properties[p][i]
             features.append({'type': 'Feature',
                              'geometry':{'type': 'Polygon', 'coordinates': [coords]},
                              'properties': properties})
         geojson_object={'type': 'FeatureCollection',
                         'features': features}
         if include_global_properties:
-            geojson_object['properties']={
-                                'geogrid_to_tui_mapping': self.geogrid_to_tui_mapping,
-                                'header': self.header}
+            type_attributes = {
+                'LBCS': [{'proportion':1,'use':{}}],
+                'NAICS':[{'proportion':1,'use':{}}],
+                'color':[0,0,0],
+                'height': 0,
+                'interactive': "Web", 
+                'name': 'test',
+                'tableData': {'id': 0}
+              }
+            geojson_object['properties']={ 
+                'geogrid_to_tui_mapping': { },
+                'header': self.header,
+                'types' : {'Default': type_attributes}
+                }
         return geojson_object
     
     def plot(self):
@@ -191,14 +134,33 @@ class Grid():
         """
         plt.scatter([g[0] for g in self.grid_coords_ll], 
             [g[1] for g in self.grid_coords_ll], c='blue', alpha=0.5)
-        if 'tui_id' in self.properties:
-            plt.scatter([self.grid_coords_ll[g][0] for g in range(len(self.grid_coords_ll)
-                        ) if self.properties['tui_id'][g] is not None], 
-                        [self.grid_coords_ll[g][1] for g in range(len(self.grid_coords_ll)
-                        ) if self.properties['tui_id'][g] is not None], 
-                        c='red', alpha=0.5)
+
         plt.show()
-        
 
 
+    def commit_grid(table_name,grid_geo, save_json = False):
+        """
+        Commits the geogrid 
+        reset_geogrid_data() generates the GEOGRIDDATA and clear_endpoints() removes any exiting traces or previous indicators
+        Saves as a json the table created
+        """
+        H = Handler(table_name, shell_mode=True)
 
+        r = requests.post(H.cityIO_post_url, data = json.dumps({'GEOGRID':grid_geo}), headers=Handler.cityio_post_headers)
+        print('Geogrid:')
+        print(r.url)
+        print(r)
+
+        H.reset_geogrid_data()
+        H.clear_endpoints()
+
+        # Save the geogrid created
+        if save_json:
+            geogrid_out_fname = 'grid_'+ table_name +'.json'
+            overwrite = input(f'Overwrite {geogrid_out_fname}? (y/n)')
+            if overwrite=='y':
+                with open(geogrid_out_fname, 'w') as f:
+                    json.dump(geogrid, f)
+
+    def edit_types(geogrid, types_json):
+        geogrid['properties']['types']=types_json
