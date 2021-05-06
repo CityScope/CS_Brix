3b675e14b254d6c5d0432f5f978cd220e058f24b
crisjf <crisjf@mit.edu>
2021-05-06 11:49:00 -0400

    exposed edit_types, and commit_grid functions to user

--- brix/__init__.py
+++ brix/__init__.py
@@ -2,4 +2,4 @@
 from .classes import *
 from .functions import *
 from .test_tools import User, spin_users, stop_users, list_users
-from .grid_maker import Grid
+from .grid_maker import Grid, commit_grid, edit_types
